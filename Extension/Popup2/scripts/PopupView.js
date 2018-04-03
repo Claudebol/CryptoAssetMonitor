69a99f16b8e131258e6adb81329b1be5565f2b29
Sammy Sherman <ssherman@iastate.edu>
2018-04-02 21:07:11 -0500

    [Scripts] Hide/show password when click on eye

--- Extension/Popup2/scripts/PopupView.js
+++ Extension/Popup2/scripts/PopupView.js
@@ -1,11 +1,29 @@
 
 $(document).ready(function(){
-  showLoader();
+  //showLoader();
+  showLogin();
+
 
   function showLoader(){
     $('#content').load('views/loader.html')
   }
   function showLogin(){
-    $('#content').load('views/login.html')
+    $('#content').load('views/login.html', function(){
+      $('.eye').click(togglePassword);
+    });
+  }
+
+
+  function togglePassword(){
+    var pword = $('#password');
+    var swap = pword.data('type');
+    pword.data('type', pword.prop('type'));
+    pword.prop('type', swap);
+
+    var eye = $('.eye');
+    swap = eye.data('title');
+    eye.data('title', eye.prop('title'));
+    eye.prop('title', swap);
+    $('.eye div').toggleClass('hide');
   }
 });
