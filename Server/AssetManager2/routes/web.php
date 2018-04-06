916168f52ce1d020a5c80e53f0653d7fe355554f
Sammy Sherman <ssherman@iastate.edu>
2018-04-06 02:07:13 -0500

    [Routes] Set up resourceful controller routes. Test auth.

--- Server/AssetManager2/routes/web.php
+++ Server/AssetManager2/routes/web.php
@@ -10,7 +10,34 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/form', function(){
+  return view('test');
+});
+
+Route::resources([
+  'cookie' => 'CookieController',
+  'currency' => 'CurrencyController',
+  'settings' => 'SettingsController',
+  'subscriber' => 'SubscriberController',
+  'subscription' => 'SubscriptionController',
+]);
+
+Route::post('/login', function(Request $request){
+
+  //echo \App\Models\Subscriber::find(37)->
+  if (Auth::attempt(array('email' => $request->input('email'), 'password' => $request->input('password')), true)){
+    Session::save();
+    return response(Auth::User());
+  }else{
+    return response(App\Models\Subscriber::where('email', $request->input('email'))->get() );
+  }
+  return response('error');
+
+
 });
